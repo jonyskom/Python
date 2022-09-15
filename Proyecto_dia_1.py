bdc2237b362c4b27ebd74236455d99187e8d2a12
jonyskom <87505712+jonyskom@users.noreply.github.com>
2022-09-14 21:49:33 -0500

    Create Proyecto_dia_1.py

---
+++ Proyecto_dia_1.py
@@ -0,0 +1,12 @@
+Saludo = "Hola Bienvenido"
+pregunta_1 = "Cual es tu nombre ?:\n"
+pregunta_2 = "Cual es tu edad ?:\n"
+pregunta_3 = "Cual es tu pais de nacimiento ?:\n"
+
+print(Saludo)
+nombre_1 = input(pregunta_1)
+edad = input(pregunta_2)
+pais = input(pregunta_3)
+
+
+print("Hola" + " " + nombre_1 + " " + "tu edad es" + " " + edad + " " + "y eres de" + " " + pais)
